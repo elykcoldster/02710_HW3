477395f56839c89ff36e6b1f9b23fb3179c761fa
Kyle Xiong <kylexiong1993@gmail.com>
2017-04-12 14:39:42 -0400

    Commented phylogeny.py

--- phylogeny.py
+++ phylogeny.py
@@ -6,11 +6,12 @@ def getScore(t, present):
 	score = 0
 	for i in range(0,2): # i = 0 for left branch, 1 for right branch
 		if isinstance(t[i], tuple):
+			# get branch scores for different presence states
 			branch_scores = [getScore(t[i], 0), getScore(t[i], 1)]
 			score += min(branch_scores)
 
 			# this step checks if a branch's optimal assignment requires it to insert/delete region x
-			# if the branch's presence can be either 0 or 1, assume it prefers to be the same
+			# if the branches can be assigned either 0 or 1, assume it prefers to be the same
 			# assignment as this node
 			if np.min(branch_scores) != np.max(branch_scores) and np.argmin(branch_scores) != present:
 				score += 1
